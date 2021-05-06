962e83b8577ce4b4fc1ea0d73dc66e81396b2dfe
Cleison França Amorim <>
2021-05-05 22:42:11 -0400

    study

--- reactStudy/src/tutorial/10-prop-types/setup/Product.js
+++ reactStudy/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <h4>Single Produtc</h4>
+      <article className="product">
+        <img src={url || defaultImage} alt={name || "default name"} />
+        <h4> {name} </h4>
+        <p> $ {price || 3.99} </p>
+      </article>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+// Product.defaultProps = {
+//   name : "default name",
+//   price: 3.99,
+//   image: defaultImage
+// }
 
 export default Product;
---
+++ reactStudy/src/tutorial/10-prop-types/setup/testing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const testing = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+testing.propTypes = {
+    name : PropTypes.array.isRequired,
+}
+
+export default testing
